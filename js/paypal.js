69d9493fb9ff492a7987552a0bfdf7acc9000efb
nitinutsav80@gmail.com <66345351+Nitin-U@users.noreply.github.com>
2022-05-15 19:59:43 +0545

    paypal jss script

---
+++ js/paypal.js
@@ -0,0 +1,31 @@
+//const amountElement = document.getElementById("amount")
+//console.log(amountElement.value)
+
+paypal.Buttons({
+	style: {
+		color: 'blue',
+		shape: 'pill'
+	},
+	createOrder:function(data,actions){
+		return actions.order.create({
+			purchase_units:[{
+				amount:{
+					value: total
+				}
+			}]
+		});
+	},
+	onApprove: function(data, actions) {
+    // This function captures the funds from the transaction.
+    return actions.order.capture().then(function(details) {
+      // This function shows a transaction success message to your buyer.
+      alert('Transaction completed by ' + details.payer.name.given_name);
+    });
+  },
+	onCancel:function(data){
+		window.location.href="fail.php";
+	}
+}).render('#paypal-payment-button');
+
+
+
