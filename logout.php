85811d26b39648977471303a349581b2a00f2574
nitinutsav80@gmail.com <66345351+Nitin-U@users.noreply.github.com>
2022-05-18 18:27:17 +0545

    alert message shown when user logs out

--- logout.php
+++ logout.php
@@ -3,6 +3,10 @@
 	session_start();
 	session_unset();
 	session_destroy();
+
+	session_start();
+	$_SESSION['passmessage']="Logged out successfully";
 	header('location: index.php');
 
+	include "footer.php";
 ?>
