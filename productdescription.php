6f41929f163042294b6cd0977a36c6da27124566
nitinutsav80@gmail.com <66345351+Nitin-U@users.noreply.github.com>
2022-05-21 21:45:32 +0545

    productdescription page contents displayed via backend

--- productdescription.php
+++ productdescription.php
@@ -8,14 +8,14 @@ include "crud/connection.php";
   <title>Product Description</title>
   <?php include "header.php"; ?>
   <?php 
-    $id=$_GET['id'];
-    $query="SELECT * FROM PRODUCT where PRODUCT_ID = ".$id;
-    $result = oci_parse($conn,$query);
-    oci_execute($result);
-    $row = oci_fetch_assoc($result);
-    $image = $row['PRODUCT_IMAGE'];
+  $id=$_GET['id'];
+  $query="SELECT * FROM PRODUCT where PRODUCT_ID = ".$id;
+  $result = oci_parse($conn,$query);
+  oci_execute($result);
+  $row = oci_fetch_assoc($result);
+  $image = $row['PRODUCT_IMAGE'];
 
-    ?>
+  ?>
 
   <style>
     #abc{
@@ -56,24 +56,24 @@ include "crud/connection.php";
     }
     /*div.zoom {
       background-image: url(<?php echo '$image'?>);
-    }*/
-    div.zoom img:hover {
-      opacity: 0;
-    }
-    div.zoom img {
-      cursor: zoom-in;
-      transition: opacity 0.5s;
-      display: block;
-      width: 100%;
-    }
-  </style>
+      }*/
+      div.zoom img:hover {
+        opacity: 0;
+      }
+      div.zoom img {
+        cursor: zoom-in;
+        transition: opacity 0.5s;
+        display: block;
+        width: 100%;
+      }
+    </style>
 
-</head>
+  </head>
 
 
-<body>
-  <div class="container mt-5">
-    
+  <body>
+    <div class="container mt-5">
+      
       <div class="row shadow-none p-3 bg-light rounded">
         <div class="zoom col-md-6 p-0" style="background-image: url(<?php echo "'img/food/".$image."'"?>)" onmousemove="zoom(event)" id="zoomImg">
           <img class="PRODUCT_IMAGE" src="img/food/<?php echo $row['PRODUCT_IMAGE'] ?>">
@@ -108,83 +108,85 @@ include "crud/connection.php";
 
         </div>
       </div>
-  </div>
-
-
-  <div class="container" id="headings-padding">
-    <div class="row">
-      <div class="col-12 text-center">
-        <h1>Top Reviews</h1>
-        <hr class="head1">
-      </div>
     </div>
-  </div>
 
-  <div class="container mb-5">
-    <div class="row mb-5 mx-3">
-      <div class="media">
-        <img class="mr-3" src="img/dummy.png" width="140" height="120" alt="Generic placeholder image">
-        <div class="media-body">
-          <h5 class="mt-0">Very nice product. <span class="text-warning"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-half-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </span></h5>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+
+    <div class="container" id="headings-padding">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1>Top Reviews</h1>
+          <hr class="head1">
         </div>
       </div>
     </div>
 
-    <div class="row mb-5 mx-3">
-      <div class="media"> <img class="mr-3" src="img/dummy.png" width="140" height="120" alt="Generic placeholder image">
-        <div class="media-body">
-          <h5 class="mt-0">Best product best material.<span class="text-warning"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> </span></h5>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </div>
+    <div class="container mb-5">
+      <div class="row mb-5 mx-3">
+        <div class="media">
+          <img class="mr-3" src="img/dummy.png" width="140" height="120" alt="Generic placeholder image">
+          <div class="media-body">
+            <h5 class="mt-0">Very nice product. <span class="text-warning"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-half-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </span></h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+      </div>
+
+      <div class="row mb-5 mx-3">
+        <div class="media"> <img class="mr-3" src="img/dummy.png" width="140" height="120" alt="Generic placeholder image">
+          <div class="media-body">
+            <h5 class="mt-0">Best product best material.<span class="text-warning"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> </span></h5>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </div>
+        </div>
       </div>
     </div>
-  </div>
 
 
-  <div class="container" id="headings-padding">
-    <div class="row">
-      <div class="col-12 text-center">
-        <h1>Give Review</h1>
-        <hr class="head2">
+    <div class="container" id="headings-padding">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1>Give Review</h1>
+          <hr class="head2">
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="container shadow-none p-3 mb-5 bg-light rounded">
-    <div class="row mb-2 mt-2 mx-3">
-      <h2> Post Your Own Reviews</h2>
+    <div class="container shadow-none p-3 mb-5 bg-light rounded">
+      <div class="row mb-2 mt-2 mx-3">
+        <h2> Post Your Own Reviews</h2>
+      </div>
+
+
+      <form>
+        <div class="form-group mx-3">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group mx-3">
+          <label for="exampleInputPassword1">Review</label>
+          <textarea type="text" class="form-control" id="exampleInputtextarea" placeholder="Write a review" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-cart mx-3" id="abc">Submit</button>
+      </form>
     </div>
 
 
-    <form>
-      <div class="form-group mx-3">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      </div>
-      <div class="form-group mx-3">
-        <label for="exampleInputPassword1">Review</label>
-        <textarea type="text" class="form-control" id="exampleInputtextarea" placeholder="Write a review" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-cart mx-3" id="abc">Submit</button>
-    </form>
-  </div>
+
+    <?php include "footer.php";
+    clearMsg();
+    ?>
+    <script type="text/javascript">
+      function zoom(e){
+        var zoomer = document.getElementById('zoomImg');
+        e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+        e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+        x = offsetX/zoomer.offsetWidth*100
+        y = offsetY/zoomer.offsetHeight*100
+        zoomer.style.backgroundPosition = x + '%' + y + '%';
+        zoomer.style.backgroundRepeat="no-repeat";
+        zoomer.style.backgroundSize = "1000px";
+      }
+    </script>
 
 
-
-  <?php include "footer.php"; ?>
-  <script type="text/javascript">
-function zoom(e){
-  var zoomer = document.getElementById('zoomImg');
-  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
-  x = offsetX/zoomer.offsetWidth*100
-  y = offsetY/zoomer.offsetHeight*100
-  zoomer.style.backgroundPosition = x + '%' + y + '%';
-  zoomer.style.backgroundRepeat="no-repeat";
-  zoomer.style.backgroundSize = "1000px";
-}
-</script>
-
-
-</body>
-</html>
+  </body>
+  </html>
