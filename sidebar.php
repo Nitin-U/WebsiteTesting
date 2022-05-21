2840935e91c23b88ff85e58a8ba9e11f0f7f52da
nitinutsav80@gmail.com <66345351+Nitin-U@users.noreply.github.com>
2022-05-21 21:41:35 +0545

    customer and trader sidebar active fixed

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,14 @@
 <div class="sidenav">
-    <a class="<?php if($_SERVER['PHP_SELF'] == '/Website/trader_profile_setting.php') echo 'active'?>" href="trader_profile_setting.php">&nbsp; <i class="fa fa-user" style="color:white;"></i> &nbsp; My Profile</a>
+    <a class="<?php if($_SERVER['PHP_SELF'] == '/Website testing/trader_profile_setting.php') echo 'active'?>" href="trader_profile_setting.php">&nbsp; <i class="fa fa-user" style="color:white;"></i> &nbsp; My Profile</a>
 
     <button class="dropdown-btn">
       &nbsp;<i class="fa fa-shopping-cart" style="color:white;"></i> &nbsp; Products<i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-container">
-      <a class="<?php if($_SERVER['PHP_SELF'] == '/Website/addProduct.php') echo 'active'?>" href="addProduct.php">Add Products</a>
+      <a class="<?php if($_SERVER['PHP_SELF'] == '/Website testing/addProduct.php') echo 'active'?>" href="addProduct.php">Add Products</a>
       <!--a href="manageShop.php">Manage Shop</a-->
       <a href="displayProduct.php">Display Products</a>
     </div>
-    <a class="<?php if($_SERVER['PHP_SELF'] == '/Website/shop_trader.php') echo 'active'?>" href="shop_trader.php">&nbsp;<i class="fa fa-shop" style="color:white;"></i> &nbsp; Shops</a>
-    <a class="<?php if($_SERVER['PHP_SELF'] == '/Website/discount.php') echo 'active'?>" href="discount.php">&nbsp;<i class="fa fa-percent" style="color:white;"></i> &nbsp; Discounts</a>
+    <a class="<?php if($_SERVER['PHP_SELF'] == '/Website testing/shop_trader.php') echo 'active'?>" href="shop_trader.php">&nbsp;<i class="fa fa-shop" style="color:white;"></i> &nbsp; Shops</a>
+    <a class="<?php if($_SERVER['PHP_SELF'] == '/Website testing/discount.php') echo 'active'?>" href="discount.php">&nbsp;<i class="fa fa-percent" style="color:white;"></i> &nbsp; Discounts</a>
   </div>
